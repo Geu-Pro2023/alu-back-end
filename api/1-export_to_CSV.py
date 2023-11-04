e99632291375e0a316a28534de52af8608dc6257
myUsername <g.bior@alustudent.com>
2023-11-04 11:24:22 -0700

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-    python script that exports data in the JSON format
+    Python script that exports data in the CSV format
 """
+import csv
 import json
 import requests
 from sys import argv
@@ -9,44 +10,44 @@ from sys import argv
 
 if __name__ == "__main__":
     """
-        request user info by employee ID
+        Request user info by employee ID
     """
     request_employee = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
+        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
     """
-        convert json to dictionary
+        Convert json to dictionary
     """
     user = json.loads(request_employee.text)
     """
-        extract username
+        Extract username
     """
     username = user.get("username")
 
     """
-        request user's TODO list
+        Request user's TODO list
     """
     request_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
     """
-        list to store task status(completed) in dictionary format
+        Dictionary to store task status(completed) in boolean format
     """
-    tasks = []
+    tasks = {}
     """
-        convert json to list of dictionaries
+        Convert json to list of dictionaries
     """
     user_todos = json.loads(request_todos.text)
     """
-        loop through dictionary & get completed tasks
+        Loop through dictionary & get completed tasks
     """
     for dictionary in user_todos:
-        task = {}
-        task["task"] = dictionary.get("title")
-        task["completed"] = dictionary.get("completed")
-        task["username"] = username
-        tasks.append(task)
+        tasks.update({dictionary.get("title"): dictionary.get("completed")})
 
     """
-        export to JSON
+        Export to CSV
     """
-    with open('{}.json'.format(argv[1]), 'w') as json_file:
-        json.dump({argv[1]: tasks}, json_file)
+    with open('{}.csv'.format(argv[1]), mode='w', newline='') as file:
+        file_editor = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
+        for k, v in tasks.items():
+            file_editor.writerow([argv[1], username, v, k])
+
+    print('Data exported to {}.csv'.format(argv[1]))
